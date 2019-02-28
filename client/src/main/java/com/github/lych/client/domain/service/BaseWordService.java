2beaa4875ce7cd9ccc3dcb5dfc583ced2db19a61
dzmitrylych <dmitrylych@gmail.com>
2019-02-28 10:40:19 +0300

    add cloud bus

--- client/src/main/java/com/github/lych/client/domain/service/BaseWordService.java
+++ client/src/main/java/com/github/lych/client/domain/service/BaseWordService.java
@@ -1,16 +1,18 @@
 package com.github.lych.client.domain.service;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.github.lych.client.application.config.WordConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BaseWordService implements WordService {
 
-    @Value("${lucky-word}")
-    private String luckyWord;
+    private final WordConfig config;
 
     @Override
     public String getWord() {
-        return luckyWord;
+        return config.getWord();
     }
 }
